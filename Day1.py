ca7f63f2705b746e3490462b49de15a61ba30a58
danielexcellsmith <danielexcellsmith@gmail.com>
2024-12-02 18:36:20 +0000

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -4,13 +4,17 @@ os.chdir("/Users/danielexcellsmith/Advent of Code 2024 - Python/")
 with open("Day1.txt", 'r') as f:
     puzzle_input = f.read()
 
-def part1(puzzle):
+def create_lists(puzzle):
     left_list = []
     right_list = []
     for line in puzzle.split('\n'):
         line_list = line.split('   ')
         left_list.append(int(line_list[0]))
         right_list.append(int(line_list[1]))
+    return left_list, right_list
+
+def part1(puzzle):
+    left_list, right_list = create_lists(puzzle)
     left_list.sort()
     right_list.sort()
     counter = 0
@@ -19,12 +23,7 @@ def part1(puzzle):
     return counter
 
 def part2(puzzle):
-    left_list = []
-    right_list = []
-    for line in puzzle.split('\n'):
-        line_list = line.split('   ')
-        left_list.append(int(line_list[0]))
-        right_list.append(int(line_list[1]))
+    left_list, right_list = create_lists(puzzle)
     counter = 0
     for num in left_list:
         counter += num * right_list.count(num)
