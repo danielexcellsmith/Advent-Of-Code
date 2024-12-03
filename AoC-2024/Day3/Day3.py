95403874138805a525228143fd735c89e6c3d922
danielexcellsmith <danielexcellsmith@gmail.com>
2024-12-03 06:46:48 +0000

    Add files via upload

---
+++ AoC-2024/Day3/Day3.py
@@ -0,0 +1,19 @@
+import re
+
+with open("input.txt", 'r') as f:
+    puzzle_input = f.read()
+
+def part1(puzzle):
+    counter = 0
+    for x, y in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', puzzle): # all multiplication instructions
+        counter += int(x) * int(y)
+    return counter
+
+def part2(puzzle):
+    counter = 0
+    for line in puzzle.split("do()"): # each line is some do() instructions, then ignore everything after a don't()
+        counter += part1(line.split("don't()")[0]) # take part before the don't()
+    return counter
+
+print(f'Part 1: {part1(puzzle_input)}')
+print(f'Part 2: {part2(puzzle_input)}')
