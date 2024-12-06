28e7f39c4860d98f71018d5c8d68134b9ffe2a46
Daniel Excell-Smith <danielexcellsmith@gmail.com>
2024-12-06 13:37:18 +0000

    Add files via upload

--- AoC-2024/Day5/Day5.py
+++ AoC-2024/Day5/Day5.py
@@ -4,18 +4,18 @@ with open("input.txt", 'r') as f:
     puzzle_input = f.read()
 
 def update_check(rules, pages):
-    for x, y in re.findall(r'(\d{2})\|(\d{2})', rules):
-        if x not in pages or y not in pages:
+    for x, y in re.findall(r'(\d{2})\|(\d{2})', rules): # Regex on rules
+        if x not in pages or y not in pages: # Skip irrelevant rules
             continue
-        if pages.index(x) > pages.index(y):
+        if pages.index(x) > pages.index(y): # Any broken rule => False
             return False
-    return True
+    return True # No broken rules
 
 def update_change(rules, pages):
     for i in range(len(pages) - 1):
-        for x, y in re.findall(r'(\d{2})\|(\d{2})', rules):
-            if pages[i] == y and pages[i + 1] == x:
-                pages[i], pages[i + 1] = x, y
+        for x, y in re.findall(r'(\d{2})\|(\d{2})', rules): # Inefficient but works
+            if pages[i] == y and pages[i + 1] == x: # Checks each page against next page
+                pages[i], pages[i + 1] = x, y # Flips pages if necessary
     return pages
 
 def part1(puzzle):
@@ -27,16 +27,16 @@ def part1(puzzle):
         if update_check(rules, pages):
             counter += int(pages[len(pages) // 2])
         else:
-            wrong_orders.append(pages)
+            wrong_orders.append(pages) # Used for Part 2
     return counter, rules, wrong_orders
 
 def part2(puzzle):
-    rules = part1(puzzle)[1]
+    rules = puzzle.split('\n\n')[0]
     updates = part1(puzzle)[2]
     counter = 0
-    for pages in updates:
+    for pages in updates: # For each incorrect update
         new_order = pages.copy()
-        while not update_check(rules, new_order):
+        while not update_check(rules, new_order): # Until each update is in the correct order
             new_order = update_change(rules, new_order)
         counter += int(new_order[len(new_order) // 2])
     return counter
