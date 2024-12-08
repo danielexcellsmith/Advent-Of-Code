d41806344f18a9ade8251ab4e6cc0791d9876444
Daniel Excell-Smith <danielexcellsmith@gmail.com>
2024-12-08 10:10:59 +0000

    Add files via upload

--- AoC-2024/Day8/Day8.py
+++ AoC-2024/Day8/Day8.py
@@ -1,5 +1,3 @@
-import math
-
 with open("input.txt", 'r') as f:
     puzzle_input = f.read()
 
@@ -8,25 +6,25 @@ def part1(puzzle):
     puzzle_matrix = []
     antinode_grid = []
     for line in puzzle_list:
-        puzzle_matrix.append(list(line))
-        antinode_grid.append(['.'] * len(line))
+        puzzle_matrix.append(list(line)) # Make list of lists grid
+        antinode_grid.append(['.'] * len(line)) # Make grid for antinodes
     for y1 in range(len(puzzle_matrix)):
-        for x1 in range(len(puzzle_matrix[0])):
-            if puzzle_matrix[y1][x1] == '.':
+        for x1 in range(len(puzzle_matrix[0])): # Iterate through all points in grid
+            if puzzle_matrix[y1][x1] == '.': # Skip if '.'
                 continue
             for y2 in range(len(puzzle_matrix)):
-                for x2 in range(len(puzzle_matrix[0])):
+                for x2 in range(len(puzzle_matrix[0])): # Iterate to find matching symbols
                     y_new, x_new = 2 * y2 - y1, 2 * x2 - x1
                     if y_new < 0 or x_new < 0 or y_new >= len(puzzle_matrix) or x_new >= len(puzzle_matrix[0]):
+                        continue # Don't leave grid
+                    if y1 == y2 and x1 == x2: # Don't place antinode where antennae are
                         continue
-                    if y1 == y2 and x1 == x2:
-                        continue
-                    if puzzle_matrix[y2][x2] == puzzle_matrix[y1][x1]:
+                    if puzzle_matrix[y2][x2] == puzzle_matrix[y1][x1]: # If symbols match
                         antinode_grid[y_new][x_new] = '#'
     counter = 0
     for line in antinode_grid:
         for char in line:
-            if char == '#':
+            if char == '#': # Count number of '#' in new grid
                 counter += 1
     return counter
 
@@ -34,32 +32,31 @@ def part2(puzzle):
     puzzle_list = puzzle.split('\n')
     puzzle_matrix = []
     antinode_grid = []
-    for line in puzzle_list:
+    for line in puzzle_list: # Same as part 1
         puzzle_matrix.append(list(line))
         antinode_grid.append(['.'] * len(line))
     for y1 in range(len(puzzle_matrix)):
-        for x1 in range(len(puzzle_matrix[0])):
+        for x1 in range(len(puzzle_matrix[0])): # Same as part 1
             if puzzle_matrix[y1][x1] == '.':
                 continue
             for y2 in range(len(puzzle_matrix)):
                 for x2 in range(len(puzzle_matrix[0])):
                     if y1 == y2 and x1 == x2:
                         continue
-                    dist_norm = math.sqrt(pow(y2 - y1, 2) + pow(x2 - x1, 2))
                     if puzzle_matrix[y2][x2] == puzzle_matrix[y1][x1]:
-                        if x2 == x1:
+                        antinode_grid[y1][x1] = '#' # Now place antinodes where antennae are
+                        antinode_grid[y2][x2] = '#'
+                        if x2 == x1: # Infinite edge case
                             m = 'inf'
                         else:
-                            m = (y2 - y1) / (x2 - x1)
-                        antinode_grid[y1][x1] = '#'
-                        antinode_grid[y2][x2] = '#'
+                            m = (y2 - y1) / (x2 - x1) # Finding gradient between antennae
                         for y_new in range(len(puzzle_matrix)):
-                            for x_new in range(len(puzzle_matrix[0])):
+                            for x_new in range(len(puzzle_matrix[0])): # Iterate through the grid again
                                 if x_new == x2:
                                     m_new = 'inf'
                                 else:
                                     m_new = (y_new - y2)/(x_new - x2)
-                                if m_new == m:
+                                if m_new == m: # If gradient between antennae = gradient between antenna and antinode
                                     antinode_grid[int(y_new)][int(x_new)] = '#'
 
     counter = 0
