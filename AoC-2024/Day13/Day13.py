6a3c8548cc0ab286af42cf2fdf5f6ccf362c923e
Daniel Excell-Smith <danielexcellsmith@gmail.com>
2024-12-13 15:34:00 +0000

    Update Day13.py

--- AoC-2024/Day13/Day13.py
+++ AoC-2024/Day13/Day13.py
@@ -8,9 +8,9 @@ def part1part2(puzzle, num): # I love regex
     counter = 0
     for a1, a2, b1, b2, x, y in re.findall(
             r'Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)', puzzle):
-        A = np.array([[int(a1), int(b1)], [int(a2), int(b2)]])
-        B = np.array([num + int(x), num + int(y)])
-        C = np.linalg.solve(A, B)
+        A = np.array([[int(a1), int(b1)], [int(a2), int(b2)]]) # Simultaneous Equations in Matrix
+        B = np.array([num + int(x), num + int(y)]) # Prize Value
+        C = np.linalg.solve(A, B) # Solves
         if round(C[0], 3).is_integer() and round(C[1], 3).is_integer(): # Only the integer solutions
             counter += C[0] * 3 + C[1]
     return int(counter)
